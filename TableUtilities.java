15876bc3b2b45583d09decd28c60bd00d29f565e
Alexandra Bozzini <alexandrabozzini@zipcodersmbp10.lan>
2019-02-11 21:20:39 -0500

    Started table utilities

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,9 +1,31 @@
  
-
+import java.util.Scanner;
 public class TableUtilities {
-    public static String getSmallMultiplicationTable() {
+   public static String getFormattedNumber(int number){
+      return String.format("%3d ", number);
+    }
+    
+    public static String getRow(int rowSize){
+        String s = "";
+        for (int i = 1; i <= rowSize; i++){
+            
+        }
         return null;
     }
+    public static String getSmallMultiplicationTable() {
+        String s = "";
+        int a;
+        int b;
+        for (a = 1; a <=4; a++){
+            for (b = 1; b <=4; b++){
+                s += " " + b + "|" + "\n";
+            }
+            s += a + "\n";
+        }
+        System.out.println(s);
+        new Scanner(System.in).next();
+        return s;
+    } 
 
     public static String getLargeMultiplicationTable() {
         return null;
